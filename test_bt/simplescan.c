0b04b08eb0f4b9627731b733ffb5ff17d8f8535d
Jonathan Chapple <robo@robodev.(none)>
2014-11-12 20:20:57 -0800

    add bluetooth test

---
+++ test_bt/simplescan.c
@@ -0,0 +1,62 @@
+/* scan for nearby bluetooth devices, lookup their name */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/socket.h>
+
+/* --- Bluetooth headers --- */
+#include <bluetooth/bluetooth.h>
+#include <bluetooth/hci.h>
+#include <bluetooth/hci_lib.h>
+
+int main(int argc, char *argv[]) 
+{
+	inquiry_info *ii = NULL;
+	int max_rsp, num_rsp;
+	int dev_id, sock, len, flags;
+	int i;
+	char addr[19] = {0};
+	char name[248] = {0};
+
+	dev_id = hci_get_route(NULL);
+	sock = hci_open_dev(dev_id);
+	if (dev_id < 0 || sock < 0)
+	{
+		perror("opening socket");
+		exit(1);
+	}
+
+	puts("Scanning for bluetooth devices...");
+
+	len = 8;
+	max_rsp = 256;
+	flags = IREQ_CACHE_FLUSH;
+	ii = (inquiry_info *) malloc(max_rsp * sizeof(inquiry_info));
+
+	num_rsp = hci_inquiry(dev_id, len, max_rsp, NULL, &ii, flags);
+	if (num_rsp < 0)
+	{
+		perror("hci_inquiry");
+	}
+
+	for (i = 0; i < num_rsp; i++)
+	{
+		//bdaddr_t contains a 6 part 8-bit integer address
+		//str2ba(const char *str, bdaddr_t *ba);
+		//ba2str(const bdaddr_t *ba, char *str);
+		ba2str(&(ii+i)->bdaddr, addr);
+		memset(name, 0, sizeof(name));
+		if (hci_read_remote_name(sock, &(ii+i)->bdaddr, sizeof(name), name, 0) < 0)
+		{
+			strcpy(name, "[unknown]");
+		}
+		
+		printf("\[%d\]%s\t%s\n", i, addr, name);
+	}
+
+
+	free(ii);
+	close(sock);
+	return 0;
+}
